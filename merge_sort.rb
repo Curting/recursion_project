dafee658ddc7785cff3ac013bd8683bcad8a1143
Oliver <oc@hejverden.dk>
2017-11-08 14:03:06 +0100

    Polish solution a bit

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,18 +1,19 @@
-def merge_sort(ary, result = [])
+def merge_sort(ary)
   if ary.length < 2
     ary # => [18] && => [6]
   else
+    result = Array.new
     half_length = (ary.length / 2.0).floor # => 1
-    #sort left hand of n
+
+    # Sort left hand of ary.length
     left_half = ary[0..half_length - 1] # => [18, 6] # => [18]
     left_half_s = merge_sort(left_half) # => [18]
 
-    #sort right hand of n
+    # Sort right hand of ary.length
     right_half = ary[half_length..-1] # => [4, 33] # => [6]
     right_half_s = merge_sort(right_half) # => [6]
 
-
-    #merge sorted halves
+    # Merge sorted halves
     ary.length.times do
       if left_half_s.empty?
         result << right_half_s.delete_at(0)
@@ -27,5 +28,32 @@ def merge_sort(ary, result = [])
   end
 end
 
+def merge_sort2(ary)
+  if ary.length < 2
+    ary
+  else
+    result = Array.new
+    half_length = (ary.length / 2.0).floor # => 1
+
+    # Sort left hand of ary.length
+    left_half_s = merge_sort(ary[0..half_length - 1]) # => [18]
+
+    # Sort right hand of ary.length
+    right_half_s = merge_sort(ary[half_length..-1]) # => [6]
+
+    # Merge sorted halves
+    ary.length.times do
+      if left_half_s.empty? || right_half_s.empty? == true
+        result << (left_half_s.empty? ? right_half_s.delete_at(0) : left_half_s.delete_at(0))
+      else
+        result << (left_half_s.first < right_half_s.first ? left_half_s.delete_at(0) : right_half_s.delete_at(0)) # => [6]
+      end
+    end
+
+    result
+  end
+end
+
 puts merge_sort([6, 12, 4, 103, 43, 10, 54, 18, 55, 22]).inspect 
+puts merge_sort2([6, 12, 4, 103, 43, 10, 54, 18, 55, 22]).inspect 
 # => [4, 6, 10, 12, 18, 22, 43, 54, 55, 103]
